31599680933e217444bea2759f0e690edeb9a0b3
Doc Ellis <DAVROGALL@ICLOUD.COM>
2018-07-28 15:34:29 -0700

    updated twitter api

--- keys.js
+++ keys.js
@@ -1,6 +1,4 @@
 
-console.log('this is loaded');
-
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -11,4 +9,8 @@ exports.twitter = {
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+  key: process.env.OMDB_API_KEY
 };
